986c6eb621d49ff24f313403238bcd3b8bf1ab2c
cosmosJ <jwj0392@gmail.com>
2024-07-11 15:33:10 +0900

    chore: clear passwordEnconder in CustomUserDetailService
    clear passwordEnconder
    
    PR:close

--- src/main/java/com/aivle/carekids/domain/user/general/service/CustomUserDetailService.java
+++ src/main/java/com/aivle/carekids/domain/user/general/service/CustomUserDetailService.java
@@ -2,29 +2,20 @@ package com.aivle.carekids.domain.user.general.service;
 
 import com.aivle.carekids.domain.user.models.Users;
 import com.aivle.carekids.domain.user.repository.UsersRepository;
-import com.aivle.carekids.global.exception.ExceptionCode;
-import com.aivle.carekids.global.exception.GlobalExceptionHandler;
-import com.aivle.carekids.global.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class CustomUserDetailService implements UserDetailsService {
     private final UsersRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Users userData = userRepository.findByUsersEmail(username);
-
-        String pw = passwordEncoder.encode(userData.getUsersPassword());
-
-        int a = 1;
 
         if (userData == null){
             throw new UsernameNotFoundException("User not registered");
